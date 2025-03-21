6f58f372d7dda949597a88ca2750a7cb9818255a
John Doe <johndoe@google.com>
2025-03-20 21:58:22 -0700

    Remove prints on succesful upload, and always make dest dir end with /

--- agentless/gcs/upload_results.py
+++ agentless/gcs/upload_results.py
@@ -1,11 +1,9 @@
 import argparse
 import os
-import tqdm
-
 from google.cloud import storage
 from google.cloud.storage import transfer_manager
 
-_BUCKET_NAME = 'agentless-results'
+_BUCKET_NAME = "agentless-results"
 
 def bulk_upload(bucket_name, source_dir, dest_dir, workers):
     storage_client = storage.Client()
@@ -25,18 +23,23 @@ def bulk_upload(bucket_name, source_dir, dest_dir, workers):
         # the input list, in order.
         if isinstance(result, Exception):
             print("Failed to upload {} due to exception: {}".format(name, result))
-        else:
-            print("Uploaded {} to {}.".format(name, bucket.name))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--bucket_name", type=str, default=_BUCKET_NAME, help="GCS bucket name")
     parser.add_argument("--source_dir", type=str, required=True, help="Local directory to upload")
     parser.add_argument("--dest_dir", type=str, required=True, help="GCS directory to upload to")
-    parser.add_argument("--workers", type=int, default=8, help="Number of workers to use for upload")
+    parser.add_argument("--num_workers", type=int, default=8, help="Number of workers to use for upload")
     args = parser.parse_args()
 
-    bulk_upload(args.bucket_name, args.source_dir, args.dest_dir, args.workers)
+    bulk_upload(
+        args.bucket_name,
+        args.source_dir,
+        # Always make the dest_dir ends with "/"
+        os.path.join(args.dest_dir, ""),
+        args.num_workers,
+    )
+
 
 if __name__ == "__main__":
     main()
